48521e15d7340a5fb6af310bd7a57fc92a979e5b
Artturin <Artturin@artturin.com>
2026-03-07 21:23:41 +0200

    Add flake and CI

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.utils.lib.eachDefaultSystem (
+      system:
+      let
+        name = "viewnior";
+        pkgs = import inputs.nixpkgs {
+          localSystem = system;
+          overlays = [
+            (final: prev: {
+              viewnior = (prev.viewnior.override { stdenv = prev.clangStdenv; }).overrideAttrs {
+                src = prev.nix-gitignore.gitignoreSource [ ] ./.;
+              };
+              viewnior-with-compile-commands = final.viewnior.overrideAttrs (_: {
+                dontPatch = true;
+                dontConfigure = true;
+                dontBuild = true;
+                dontFixup = true;
+                installPhase = ''
+                  meson setup build
+                  mkdir -p $out/.build
+                  # This will cause the output to depend on all the buildInputs
+                  cp ./build/compile_commands.json $out/.build
+                '';
+              });
+            })
+          ];
+        };
+      in
+      {
+        packages = {
+          default = inputs.self.packages."${system}".${name};
+          "${name}" = pkgs.${name};
+          viewnior-with-compile-commands = pkgs.viewnior-with-compile-commands;
+        };
+
+        devShells.default =
+          with pkgs;
+          mkShell.override { stdenv = pkgs.viewnior.stdenv; } {
+            inputsFrom = [ inputs.self.packages."${system}".${name} ];
+            nativeBuildInputs = [ clang-tools ];
+            env = {
+              CLANGD_FLAGS = "--query-driver=${pkgs.lib.getExe clangStdenv.cc}";
+            };
+            shellHook = ''
+              if [ ! -f compile_commands.json ]; then
+                echo "Generating compile_commands.json..."
+                nix build ".#viewnior-with-compile-commands" -o result-compile-commands
+                # When directly linking without replacing directory `vnr-prefs.h` has `config.h file not found`
+                #ln -sf "$(chase ./result-compile-commands)/.build/compile_commands.json" .
+                cp ./result-compile-commands/.build/compile_commands.json .
+                sed -i "/directory\":/c\"directory\": \"$PWD/build\"," compile_commands.json
+                rm -f result-compile-commands
+              fi
+            '';
+          };
+
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
+}
